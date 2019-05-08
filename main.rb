a05d4c2c5ffdb2e29e8404dc2e839054d25ddd82
Noordeen Malango <noordinmalango@gmail.com>
2019-05-08 17:38:04 +0200

    Game logic

--- main.rb
+++ main.rb
@@ -1,13 +1,15 @@
 class Player
-    attr_accessor :symbol, :name, :moves 
+    attr_accessor :symbol, :name, :moves
     def initialize(name)
         @name = name
+        @moves = []
     end
 end
 class Game
-    attr_accessor :board, :player_1, :player_2
+    attr_accessor :board, :player_1, :player_2, :turns
     def initialize(board)
         @board = board
+        @turns = 1
     end
     def display_board
         puts " #{@board[0]} ┃ #{@board[1]} ┃ #{@board[2]}"
@@ -16,11 +18,8 @@ class Game
         puts "━━━╋━━━╋━━━"
         puts " #{@board[6]} ┃ #{@board[7]} ┃ #{@board[8]}"
     end
-    def board
-        
-    end
     def play
-        display_board 
+        display_board
         puts "Enter name for player1:"
         player_name = gets.chomp
         @player_1 = Player.new(player_name)
@@ -37,9 +36,45 @@ class Game
         end
         @player_1.symbol = symbol
         @player_1.symbol == "X" ? @player_2.symbol = "O" : @player_2.symbol = "X"
-    end
-    def move
+        print "#{@player_1.name} your symbol is #{@player_1.symbol} and "
+        puts "#{@player_2.name} your symbol is #{@player_2.symbol}"
+        while !over?
+            if(@turns%2 !=0)
+              move(@player_1)
+            else
+              move(@player_2)
+            end
+        end
 
+    end
+    def move(player)
+        puts "#{player.name} please choose the position to put your symbol: "
+        choice = gets.chomp.to_i
+        while (!@board.include?(choice)) || (!@board[choice-1].is_a? Integer)
+           puts "Wrong choice! You need to choose a number between 1  and 9 that is available on the board."
+           display_board
+           puts "#{player.name} please choose a valid position: "
+           choice = gets.chomp.to_i
+        end
+        @turns += 1
+        @board[choice-1] = player.symbol
+        player.moves << choice
+        display_board
+    end
+    def over?
+       if line?
+         puts "#{@turns % 2 == 1 ? "#{@player_2.name}" : "#{@player_1.name}"} won!"
+         return true
+       elsif @turns > 9
+         puts "It's a tie!"
+         return true
+       else
+         return false
+       end
+    end
+    def line?
+      lines = [ [1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7] ]
+      (@player_1.moves.permutation(3).to_a & lines).length > 0 or (@player_2.moves.permutation(3).to_a & lines).length > 0
     end
 end
 
